2d8772d102acf379d5bfa3928883adf695a74fcf
unknown <xKapellMeisterx@yandex.ru>
2022-03-15 19:26:15 +0300

    Review_2

--- homework.py
+++ homework.py
@@ -1,28 +1,24 @@
+from dataclasses import dataclass, asdict
+
+
+@dataclass
 class InfoMessage:
-    """Базовый класс тренировки."""
+    """Информация о тренировке."""
 
-    def __init__(self,
-                 training_type: str,
-                 duration: float,
-                 distance: float,
-                 speed: float,
-                 calories: float,
-                 ) -> None:
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: float
+    MESSAGE: str = ('Тип тренировки: {training_type}; '
+                    'Длительность: {duration:.3f} ч.; '
+                    'Дистанция: {distance:.3f} км; '
+                    'Ср. скорость: {speed:.3f} км/ч; '
+                    'Потрачено ккал: {calories:.3f}.')
 
-    def get_message(self) -> str:
-        """Информация о выполненной тренировке."""
-        return (
-            f'Тип тренировки: {self.training_type};'
-            f' Длительность: {self.duration:.3f} ч.;'
-            f' Дистанция: {self.distance:.3f} км;'
-            f' Ср. скорость: {self.speed:.3f} км/ч;'
-            f' Потрачено ккал: {self.calories:.3f}.'
-        )
+    def get_message(self):
+        """Наполнение информации о тренировке в MESSAGE."""
+        return self.MESSAGE.format(**asdict(self))
 
 
 class Training:
@@ -51,7 +47,8 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        pass
+        raise NotImplementedError(
+            'Определите \"def get_spent_calories\" в наследнике')
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
@@ -145,6 +142,8 @@ def read_package(workout_type: str, data: list) -> Training:
         'WLK': SportsWalking
     }
 
+    if workout_type not in class_data:
+        raise KeyError('Неизвестный тип тренировки')
     return class_data[workout_type](*data)
 
 
